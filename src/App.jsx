39b19d179e501cf85b3eaf9e90f0a70a4ab37268
Engelbert Frites <engelfreites97@gmail.com>
2023-06-12 17:11:53 -0500

    creacion del componente titulo y se agrego las imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './App.css'
 import Header from './component/header/Header'
+import img from './assets/initial.jpg'
+import model2 from './assets/model2.jpeg'
+import { FaStoreAlt } from 'react-icons/fa'
+import { Title } from './component/Title/Title'
+import model from './assets/model.jpg'
 
 function App () {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <div className='content grid'>
+        <div className='bar col-12 '>
+          <FaStoreAlt />
+          Mi tienda online
+        </div>
+        <div className='content-img full-width'>
+          <img src={img} alt='inital-img'/>
+        </div>
+          <Title>Recomendacion</Title>
+          <div className='recomendaciones full-width' >
+            <img src={model} alt='model' className='recomendacion' />
+            <img src={model2} alt='model' className='recomendacion' />
+          </div>
       </div>
       <footer>footer</footer>
     </div>
