2bb080d85d51069eccee5f76b42a071faf2f8099
Engelbert Frites <engelfreites97@gmail.com>
2023-06-25 22:38:21 -0500

    test: Create second test about shoul be specific text

--- src/component/Footer/footer.cy.jsx
+++ src/component/Footer/footer.cy.jsx
@@ -1,8 +1,13 @@
 import { Footer } from "./Footer"
 
-
 describe('footer', () => {
+
   it('the component can render', () => {
     cy.mount(<Footer/>)
+  }),
+
+  it('shoul be the specific text content', ()=>{
+    cy.mount(<Footer />)
+    cy.contains('Creado por Engel')
   })
 })
