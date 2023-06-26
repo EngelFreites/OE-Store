e5dd00fcd00e5de84d891032861cd0ab8b446985
Engelbert Frites <engelfreites97@gmail.com>
2023-06-26 10:42:11 -0500

    feat: add new content for icons socail networks

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,12 +1,16 @@
 import {AiFillFacebook,AiFillInstagram, AiFillGithub } from 'react-icons/ai'
 import './footer.css'
+
 export function Footer () {
   return (
     <footer>
-      Creado por Engel  
-      <AiFillFacebook data-test="facebook" />
-      <AiFillInstagram data-test="instagram" />
-      <AiFillGithub data-test="github" />
+      Creado por Engel
+      <div className='content-footer'>
+        <AiFillFacebook data-test="facebook" />
+        <AiFillInstagram data-test="instagram" />
+        <AiFillGithub data-test="github" />
+      </div> 
+
     </footer>
   )
 }
