669ebc5bbae19e766b462e92f0e0ed30de9c79d8
Engelbert Frites <engelfreites97@gmail.com>
2023-07-04 13:02:51 -0500

    feat: agg icons card and like

--- src/component/Product/Producto.jsx
+++ src/component/Product/Producto.jsx
@@ -1,4 +1,5 @@
 import "./product.css"
+import {AiOutlineHeart, AiOutlineShoppingCart} from 'react-icons/ai'
 
 export default function Product ({id, image, title, price}) {
   return(
@@ -6,7 +7,11 @@ export default function Product ({id, image, title, price}) {
       <img src={image} alt={title} width='180' height='220' />
       <div className="content-description"> 
         <p>{title}</p>
-        <p>{price}</p>
+        <div className="prices-icons">
+          <p>{price}</p>
+          <AiOutlineShoppingCart />
+          <AiOutlineHeart />
+        </div>
       </div>
     </div>
   )
