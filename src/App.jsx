6c36c4b124ba899631e9fa3fcfb966c8a88083d6
Engelbert Frites <engelfreites97@gmail.com>
2023-07-03 22:11:19 -0500

    feat: create function fecth for api fake shoping

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Header from './component/header/Header'
 import { Footer } from './component/Footer/Footer'
@@ -5,6 +6,15 @@ import { Footer } from './component/Footer/Footer'
 import Products from './component/Products/Products'
 
 function App () {
+  const [products, setProducts] = useState([])
+
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+      .then(res=>res.json())
+      .then(json=> setProducts(json))
+  },[products])
+
+
   return (
     <div className="App">
       <Header />
