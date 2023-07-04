6df93e33fb7d3b722610d690bfdcbc81ea73bbe0
Engelbert Frites <engelfreites97@gmail.com>
2023-07-03 22:44:37 -0500

    feat: create list of prodcts

--- src/component/Products/Products.jsx
+++ src/component/Products/Products.jsx
@@ -1,11 +1,19 @@
-
+import './products.css'
 export default function Products  ({products}){
   return(
-    <div>
+    <div className='products'>
       <ul>
         {
           products.map( product => (
-            <li key={product.id}>{product.title}</li>
+            <li key={product.id}>
+              <div className="content-product">
+                <img src={product.image} alt={product.title}  width='250' height='300'/>
+                <div className="content-description">
+                  <p>{product.title}</p>
+                  <p>{product.price}</p>
+                </div>
+              </div>
+            </li>
           ))
         }
       </ul>
