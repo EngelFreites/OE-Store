036627b3a75bdd6314a916cd56950c5762e9b444
Engelbert Frites <engelfreites97@gmail.com>
2023-07-04 13:15:42 -0500

    feat: create linea

--- src/component/Product/Producto.jsx
+++ src/component/Product/Producto.jsx
@@ -5,6 +5,7 @@ export default function Product ({id, image, title, price}) {
   return(
     <div className="content-product">
       <img src={image} alt={title} width='180' height='220' />
+      <div className="line"></div>
       <div className="content-description"> 
         <p>{title}</p>
         <div className="prices-icons">
